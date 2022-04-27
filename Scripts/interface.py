7102c337cd4f1b9a0645d6482b4c0b262bc42e31
Flodagnas <74758873+Flodagnas@users.noreply.github.com>
2022-04-27 14:19:13 +0200

    Update interface.py

--- Scripts/interface.py
+++ Scripts/interface.py
@@ -1,48 +1,75 @@
+from ast import If
 from tkinter import *
 from tkinter.font import Font
+from tkinter import messagebox
 
 def accueil():
-    global boutarcade,boutsport,boutsalon,boutmystere,boutmenu,boutpartierapide,boutcredit,boutjouer,boutleaderboard,bouttournois,boutquitter,boutathletisme,boutbasket,boutmonopoly,boutshifumi,boutdame,boutretour,menujouer,menupartierapide,menuprincipal,menutournois
+    global boutJouer,boutInscription,boutjouer,boutConnection,boutquitter,menujouer,menuInscription,menuAccueil,menuConnection
     destruction()
     dessin.itemconfigure( titre1 , text="Accueil")
 
 
-    boutpartierapide = Button(fenetre,text='Partie Rapide', width =11, command=partierapide, font=ftComic,bg="black",fg="blue")
-    boutpartierapide.grid (row =7,column = 3, padx = 10,pady = 10, columnspan =3)
-    bouttournois = Button(fenetre,text='Tournois', width =11, command=tournois, font=ftComic,bg="black",fg="blue")
-    bouttournois.grid (row =7,column =9, padx = 10,pady = 10, columnspan =3)
-    boutmenu = Button(fenetre,text='Menu Principale', width =11, command=menuprincipale, font=ftComic,bg="black",fg="blue")
-    boutmenu.grid (row =12,column =6, padx =10,pady = 10, columnspan =3)
+    boutInscription = Button(fenetre,text='Inscription', width =11, command=inscription, font=ftComic,bg="black",fg="blue")
+    boutInscription.grid (row =7,column = 3, padx = 10,pady = 10, columnspan =3)
+    boutConnection = Button(fenetre,text='Connection', width =11, command=connection, font=ftComic,bg="black",fg="blue")
+    boutConnection.grid (row =7,column =9, padx = 10,pady = 10, columnspan =3)
+    boutJouer = Button(fenetre,text='Jouer', width =11, command=jouer, font=ftComic,bg="black",fg="blue")
+    boutJouer.grid (row =12,column =6, padx =10,pady = 10, columnspan =3)
 
-    menuprincipal=False
+    menuAccueil=True
+    menujouer=False
+    menuInscription=False
+    menuConnection=False
+
+def inscription():
+    global l1,l2, l3, e1, e2, rb1, rb2, b, menujouer,menuInscription,menuAccueil,menuConnection
+    fenetrePrincipale = Tk()
+    l1 = Label(fenetrePrincipale, text = "Pseudo").grid(row = 0, column = 0)
+    e1 = Entry(fenetrePrincipale).grid(row = 0, column = 1)
+    b = Button(fenetrePrincipale ,text="Submit", command = inscription).grid(row = 5, column = 1)
+    if b == True:
+        messagebox.showinfo("Statut de l'inscription", "Formulaire Envoyé")
+        jouer()
+
+    menuAccueil=False
     menujouer=True
-    menupartierapide=False
-    menutournois=False
+    menuInscription=False
+    menuConnection=False
+
+def connection():
+    pass
+
+def jouer():
+    global boutRejoindre, boutInvite, boutRetour,menujouer,menuInscription,menuAccueil,menuConnection
+    destruction()
+    dessin.itemconfigure( titre1 , text="Jouer")
+
+    boutRejoindre = Button(fenetre,text='Rejoindre', width =11, command=rejoindre, font=ftComic,bg="black",fg="blue")
+    boutRejoindre.grid (row =7,column = 3, padx = 10,pady = 10, columnspan =3)
+    boutInvite = Button(fenetre,text='Invite', width =11, command=invite, font=ftComic,bg="black",fg="blue")
+    boutInvite.grid (row =7,column =9, padx = 10,pady = 10, columnspan =3)
+
+    boutRetour = Button(fenetre,text='Retour', width =11, command=jouer, font=ftComic,bg="black",fg="blue")
+    boutRetour.grid (row =15,column =6, padx = 10,pady = 10, columnspan =3)
+
+    menuAccueil=False
+    menujouer=True
+    menuInscription=False
+    menuConnection=False
+
+def rejoindre():
+    pass
+
+def invite():
+    pass
+
 
 def destruction():
-    global boutarcade,boutsport,boutsalon,boutmystere,boutmenu,boutpartierapide,boutcredit,boutjouer,boutleaderboard,bouttournois,boutquitter,boutjeuarcade,boutathletisme,boutjeusport,boutbasket,boutmonopoly,boutshifumi,boutdame,boutretour,menujouer,menupartierapide,menuprincipal,menutournois
-    if menuprincipal==True:
-        boutjouer.destroy()
-        boutleaderboard.destroy()
-        boutcredit.destroy()
-        boutquitter.destroy()
-    if menujouer==True:
-        bouttournois.destroy()
-        boutpartierapide.destroy()
-        boutmenu.destroy()
-    if menupartierapide==True:
-        boutathletisme.destroy()
-        boutbasket.destroy()
-        boutmonopoly.destroy()
-        boutshifumi.destroy()
-        boutdame.destroy()
-        boutretour.destroy()
-    if menutournois==True:
-        boutarcade.destroy()
-        boutsport.destroy()
-        boutsalon.destroy()
-        boutmystere.destroy()
-        boutretour.destroy()
+    global boutJouer,boutInscription,boutcredit,boutjouer,boutleaderboard,boutConnection,boutquitter,menujouer,menuInscription,menuConnection
+    if menuAccueil==True:
+        boutConnection.destroy()
+        boutInscription.destroy()
+        boutJouer.destroy()
 
 Xmax,Ymax=1500,1000
 fenetre = Tk()
@@ -66,5 +93,6 @@ boutcredit = Button(fenetre,text='Connection', width =11, command=connection, fo
 boutcredit.grid (row = 14,column = 6, padx = 10,pady = 5, columnspan =3)
 boutquitter=Button(fenetre,text='Quitter', width =10, command=fenetre.destroy, font=ftComic,bg="black",fg="blue")
 boutquitter.grid (row = 16,column = 6, padx = 10,pady = 10, columnspan =3)
+
 
 fenetre.mainloop()
