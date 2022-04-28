d38e33fe08001138df83696c8439315a594d9ee2
Flodagnas <74758873+Flodagnas@users.noreply.github.com>
2022-04-28 16:26:14 +0200

    ajout class + debug auth

--- Scripts/inscription.py
+++ Scripts/inscription.py
@@ -2,108 +2,108 @@ from tkinter import *
 from tkinter import messagebox
 import sqlite3
 
-def clear():
-	userentry.delete(0,END)
-	passentry.delete(0,END)
+class signup:
+    def clear():
+        userentry.delete(0,END)
+        passentry.delete(0,END)
 
-def close():
-	winsignup.destroy()	
+    def close():
+        winsignup.destroy()	
 
-#----------------------------------------------------------- Signup Window --------------------------------------------------
-def action():
-    if username.get()=="" or password.get()=="" or verif_pass.get()=="":
-        messagebox.showerror("Error" , "All Fields Are Required" , parent = winsignup)
-    elif password.get() != verif_pass.get():
-        messagebox.showerror("Error" , "Password & Confirm Password Should Be Same" , parent = winsignup)
-    else:
-        try:
-            con = sqlite3.connect('BDD.db')
-            cur = con.cursor()
-            cur.execute("select * from user where Pseudo=?",username.get())
-            row = cur.fetchone()
-            if row!=None:
-                messagebox.showerror("Error" , "User Name Already Exits", parent = winsignup)
-            else:
-                cur.execute("insert into user(Psuedo,HashPassword) values(?,?)",(username.get(), password.get()))
-                con.commit()
-                con.close()
-                messagebox.showinfo("Success" , "Ragistration Successfull" , parent = winsignup)
-                clear()
-                switch()
-            
-        except Exception as es:
-            messagebox.showerror("Error" , f"Error : {str(es)}", parent = winsignup)
+    #----------------------------------------------------------- Signup Window --------------------------------------------------
+    def action():
+        if username.get()=="" or password.get()=="" or verif_pass.get()=="":
+            messagebox.showerror("Error" , "All Fields Are Required" , parent = winsignup)
+        elif password.get() != verif_pass.get():
+            messagebox.showerror("Error" , "Password & Confirm Password Should Be Same" , parent = winsignup)
+        else:
+            try:
+                con = sqlite3.connect('BDD.db')
+                cur = con.cursor()
+                cur.execute("select * from user where Pseudo=?",username.get())
+                row = cur.fetchone()
+                if row!=None:
+                    messagebox.showerror("Error" , "User Name Already Exits", parent = winsignup)
+                else:
+                    cur.execute("insert into user(Psuedo,HashPassword) values(?,?)",(username.get(), password.get()))
+                    con.commit()
+                    con.close()
+                    messagebox.showinfo("Success" , "Ragistration Successfull" , parent = winsignup)
+                    clear()
+                    switch()
+                
+            except Exception as es:
+                messagebox.showerror("Error" , f"Error : {str(es)}", parent = winsignup)
 
-# close signup function			
-def switch():
-    winsignup.destroy()
+    # close signup function			
+    def switch():
+        winsignup.destroy()
 
-# clear data function
-def clear():
-    username.delete(0,END)
-    password.delete(0,END)
-    verif_pass.delete(0,END)
-
-
-# start Signup Window	
-
-winsignup = Tk()
-winsignup.title("App")
-winsignup.maxsize(width=500 ,  height=500)
-winsignup.minsize(width=500 ,  height=500)
+    # clear data function
+    def clear():
+        username.delete(0,END)
+        password.delete(0,END)
+        verif_pass.delete(0,END)
 
 
-#heading label
-heading = Label(winsignup , text = "Signup" , font = 'Verdana 20 bold')
-heading.place(x=60 , y=150)
+    # start Signup Window	
 
-username = Label(winsignup, text= "User Name :" , font='Verdana 10 bold')
-username.place(x=60,y=220)
+    winsignup = Tk()
+    winsignup.title("App")
+    winsignup.maxsize(width=500 ,  height=500)
+    winsignup.minsize(width=500 ,  height=500)
 
-password = Label(winsignup, text= "Password :" , font='Verdana 10 bold')
-password.place(x=60,y=260)
+    #heading label
+    heading = Label(winsignup , text = "Signup" , font = 'Verdana 20 bold')
+    heading.place(x=60 , y=150)
 
-verif_pass = Label(winsignup, text= "Verify Password:" , font='Verdana 10 bold')
-verif_pass.place(x=60,y=300)
+    username = Label(winsignup, text= "User Name :" , font='Verdana 10 bold')
+    username.place(x=60,y=220)
 
-# Entry Box ------------------------------------------------------------------
+    password = Label(winsignup, text= "Password :" , font='Verdana 10 bold')
+    password.place(x=60,y=260)
 
-username = StringVar()
-password = StringVar()
-verif_pass = StringVar()
+    verif_pass = Label(winsignup, text= "Verify Password:" , font='Verdana 10 bold')
+    verif_pass.place(x=60,y=300)
 
+    # Entry Box ------------------------------------------------------------------
 
-username = Entry(winsignup, width=40,textvariable = username)
-username.place(x=200 , y=223)
-
-
-password = Entry(winsignup, width=40, show="*", textvariable = password)
-password.place(x=200 , y=260)
-
-
-verif_pass= Entry(winsignup, width=40 ,show="*" , textvariable = verif_pass)
-verif_pass.place(x=200 , y=300)
-
-userentry = Entry(winsignup, width=40 , textvariable = username)
-userentry.focus()
-userentry.place(x=200 , y=223)
-
-passentry = Entry(winsignup, width=40, show="*" ,textvariable = password)
-passentry.place(x=200 , y=260)
+    username = StringVar()
+    password = StringVar()
+    verif_pass = StringVar()
 
 
-# button login and clear
-
-btn_signup = Button(winsignup, text = "Signup" ,font='Verdana 10 bold', command = action)
-btn_signup.place(x=200, y=413)
+    username = Entry(winsignup, width=40,textvariable = username)
+    username.place(x=200 , y=223)
 
 
-btn_login = Button(winsignup, text = "Clear" ,font='Verdana 10 bold' , command = clear)
-btn_login.place(x=280, y=413)
+    password = Entry(winsignup, width=40, show="*", textvariable = password)
+    password.place(x=200 , y=260)
 
 
-sign_up_btn = Button(winsignup , text="Switch To Login" , command = switch )
-sign_up_btn.place(x=350 , y =20)
+    verif_pass= Entry(winsignup, width=40 ,show="*" , textvariable = verif_pass)
+    verif_pass.place(x=200 , y=300)
+
+    userentry = Entry(winsignup, width=40 , textvariable = username)
+    userentry.focus()
+    userentry.place(x=200 , y=223)
+
+    passentry = Entry(winsignup, width=40, show="*" ,textvariable = password)
+    passentry.place(x=200 , y=260)
 
 
-winsignup.mainloop()
+    # button login and clear
+
+    btn_signup = Button(winsignup, text = "Signup" ,font='Verdana 10 bold', command = action)
+    btn_signup.place(x=200, y=413)
+
+
+    btn_login = Button(winsignup, text = "Clear" ,font='Verdana 10 bold' , command = clear)
+    btn_login.place(x=280, y=413)
+
+
+    sign_up_btn = Button(winsignup , text="Switch To Login" , command = switch )
+    sign_up_btn.place(x=350 , y =20)
+
+
+    winsignup.mainloop()
--- Scripts/interface.py
+++ Scripts/interface.py
@@ -1,48 +1,61 @@
 from tkinter import *
 from tkinter.font import Font
-import subprocess
+import connexion
+import inscription
 
 def accueil():
-    global boutJouer,boutInscription,boutjouer,boutConnection,boutquitter,menujouer,menuInscription,menuAccueil,menuConnection
-    destruction()
+    global boutJouer,boutInscription,boutConnection,boutquitter,menujouer,menuInscription,menuAccueil,menuConnection, menuInviter, menuRejoindre
     dessin.itemconfigure( titre1 , text="Accueil")
 
+    boutInscription = Button(fenetre,text='Inscription', width =11, command=inscription, font=ftComic,bg="#D1D1D1",fg="#FF9E3D")
+    boutInscription.grid (row =12,column =6, padx = 10,pady =5, columnspan =3)
+    boutConnection = Button(fenetre,text='Connection', width =11, command=connection, font=ftComic,bg="#D1D1D1",fg="#FF9E3D")
+    boutConnection.grid (row = 13,column = 6, padx = 10,pady = 5, columnspan =3)
+    boutJouer = Button(fenetre,text='Jouer', width =11, command=jouer, font=ftComic,bg="#D1D1D1",fg="#FF9E3D")
+    boutJouer.grid (row = 14,column = 6, padx = 10,pady = 5, columnspan =3)
+    boutquitter=Button(fenetre,text='Quitter', width =10, command=fenetre.destroy, font=ftComic,bg="#D1D1D1",fg="#FF9E3D")
+    boutquitter.grid (row = 15,column = 6, padx = 10,pady = 10, columnspan =3)
 
-    boutInscription = Button(fenetre,text='Inscription', width =11, command=inscription, font=ftComic,bg="black",fg="blue")
-    boutInscription.grid (row =7,column = 3, padx = 10,pady = 10, columnspan =3)
-    boutConnection = Button(fenetre,text='Connection', width =11, command=connection, font=ftComic,bg="black",fg="blue")
-    boutConnection.grid (row =7,column =9, padx = 10,pady = 10, columnspan =3)
-    boutJouer = Button(fenetre,text='Jouer', width =11, command=jouer, font=ftComic,bg="black",fg="blue")
-    boutJouer.grid (row =12,column =6, padx =10,pady = 10, columnspan =3)
+    boutRejoindre.destroy()
+    boutInvite.destroy()
+    boutRetour.destroy()
 
     menuAccueil=True
     menujouer=False
     menuInscription=False
     menuConnection=False
+    menuRejoindre=False
+    menuInviter=False
+
 
 def inscription():
-    subprocess.call("inscription.py")
+    signup
 
 def connection():
-    subprocess.call("connexion.py")
+    login
 
 def jouer():
-    global boutRejoindre, boutInvite, boutRetour, menujouer, menuInscription, menuAccueil, menuConnection
-    destruction()
+    global boutRejoindre, boutInvite, boutRetour, menujouer, menuInscription, menuAccueil, menuConnection, menuRejoindre, menuInviter
     dessin.itemconfigure( titre1 , text="Jouer")
 
-    boutRejoindre = Button(fenetre,text='Rejoindre', width =11, command=rejoindre, font=ftComic,bg="black",fg="blue")
+    boutRejoindre = Button(fenetre,text='Rejoindre', width =11, command=rejoindre, font=ftComic,bg="#D1D1D1",fg="#FF9E3D")
     boutRejoindre.grid (row =7,column = 3, padx = 10,pady = 10, columnspan =3)
-    boutInvite = Button(fenetre,text='Invite', width =11, command=invite, font=ftComic,bg="black",fg="blue")
+    boutInvite = Button(fenetre,text='Invite', width =11, command=invite, font=ftComic,bg="#D1D1D1",fg="#FF9E3D")
     boutInvite.grid (row =7,column =9, padx = 10,pady = 10, columnspan =3)
-
-    boutRetour = Button(fenetre,text='Retour', width =11, command=jouer, font=ftComic,bg="black",fg="blue")
+    boutRetour = Button(fenetre,text='Retour', width =11, command=accueil, font=ftComic,bg="#D1D1D1",fg="#FF9E3D")
     boutRetour.grid (row =15,column =6, padx = 10,pady = 10, columnspan =3)
+
+    boutJouer.destroy()
+    boutInscription.destroy()
+    boutConnection.destroy()
+    boutquitter.destroy()
 
     menuAccueil=False
     menujouer=True
     menuInscription=False
     menuConnection=False
+    menuRejoindre=False
+    menuInviter=False
 
 def rejoindre():
     pass
@@ -50,17 +63,6 @@ def rejoindre():
 def invite():
     pass
 
-
-def destruction():
-    global boutJouer,boutInscription,boutcredit,boutjouer,boutleaderboard,boutConnection,boutquitter,menujouer,menuInscription,menuConnection, menuAccueil
-    if menuAccueil==True:
-        boutConnection.destroy()
-        boutInscription.destroy()
-        boutJouer.destroy()
-    if menujouer==True:
-        boutRejoindre.destroy()
-        boutInvite.destroy()
-        boutRetour.destroy()
 
 
 fenetre = Tk()
@@ -78,17 +80,22 @@ ftComic2 = Font (family = 'Comic Sans MS', size = 50, underline = True, weight =
 ftComic3 = Font (family = 'Comic Sans MS', size = -20, underline = False, weight = "bold", slant="italic")
 
 selfX1,selfY1=Xmax/2,40;100
-titre1= dessin.create_text(selfX1,selfY1,text='Projet Matchmaking',fill='Blue', font= ftComic2)
+titre1= dessin.create_text(selfX1,selfY1,text='Projet Matchmaking',fill='Red', font= ftComic2)
 
-boutjouer = Button(fenetre,text='Accueil', width =10, command=accueil, font=ftComic,bg="#D1D1D1",fg="#FF9E3D")
-boutjouer.grid (row = 10,column = 6, padx = 10,pady = 10, columnspan =3)
-boutleaderboard = Button(fenetre,text='Inscription', width =11, command=inscription, font=ftComic,bg="#D1D1D1",fg="#FF9E3D")
-boutleaderboard.grid (row =12,column =6, padx = 10,pady =5, columnspan =3)
-boutcredit = Button(fenetre,text='Connection', width =11, command=connection, font=ftComic,bg="#D1D1D1",fg="#FF9E3D")
-boutcredit.grid (row = 13,column = 6, padx = 10,pady = 5, columnspan =3)
-boutcredit = Button(fenetre,text='Jouer', width =11, command=jouer, font=ftComic,bg="#D1D1D1",fg="#FF9E3D")
-boutcredit.grid (row = 14,column = 6, padx = 10,pady = 5, columnspan =3)
+boutInscription = Button(fenetre,text='Inscription', width =11, command=inscription, font=ftComic,bg="#D1D1D1",fg="#FF9E3D")
+boutInscription.grid (row =12,column =6, padx = 10,pady =5, columnspan =3)
+boutConnection = Button(fenetre,text='Connection', width =11, command=connection, font=ftComic,bg="#D1D1D1",fg="#FF9E3D")
+boutConnection.grid (row = 13,column = 6, padx = 10,pady = 5, columnspan =3)
+boutJouer = Button(fenetre,text='Jouer', width =11, command=jouer, font=ftComic,bg="#D1D1D1",fg="#FF9E3D")
+boutJouer.grid (row = 14,column = 6, padx = 10,pady = 5, columnspan =3)
 boutquitter=Button(fenetre,text='Quitter', width =10, command=fenetre.destroy, font=ftComic,bg="#D1D1D1",fg="#FF9E3D")
 boutquitter.grid (row = 15,column = 6, padx = 10,pady = 10, columnspan =3)
+
+menuAccueil=True
+menujouer=False
+menuInscription=False
+menuConnection=False
+menuRejoindre=False
+menuInviter=False
 
 fenetre.mainloop()
