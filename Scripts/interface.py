b7c429fe2d8f623f6f39df4340a3cbf1dc99a2fb
Flodagnas <74758873+Flodagnas@users.noreply.github.com>
2022-05-02 16:20:00 +0200

    auth

--- Scripts/interface.py
+++ Scripts/interface.py
@@ -1,13 +1,13 @@
 from tkinter import *
 from tkinter.font import Font
-import connexion
-import inscription
+from tkinter import messagebox
+import sqlite3
 
 def accueil():
-    global boutJouer,boutInscription,boutConnection,boutquitter,menujouer,menuInscription,menuAccueil,menuConnection, menuInviter, menuRejoindre
+    global boutJouer,boutInscription,boutConnection,boutquitter, Pseudo, mdp, Vérif_mdp, Submit
     dessin.itemconfigure( titre1 , text="Accueil")
 
-    boutInscription = Button(fenetre,text='Inscription', width =11, command=inscription, font=ftComic,bg="#D1D1D1",fg="#FF9E3D")
+    boutInscription = Button(fenetre,text='Inscription', width =11, command=SignUp, font=ftComic,bg="#D1D1D1",fg="#FF9E3D")
     boutInscription.grid (row =12,column =6, padx = 10,pady =5, columnspan =3)
     boutConnection = Button(fenetre,text='Connection', width =11, command=connection, font=ftComic,bg="#D1D1D1",fg="#FF9E3D")
     boutConnection.grid (row = 13,column = 6, padx = 10,pady = 5, columnspan =3)
@@ -19,23 +19,117 @@ def accueil():
     boutRejoindre.destroy()
     boutInvite.destroy()
     boutRetour.destroy()
+    Pseudo.destroy()
+    entry_pseudo.destroy()
+    mdp.destroy()
+    entry_mdp.destroy()
+    Vérif_mdp.destroy()
+    entry_Vérif.destroy()
+    Submit.destroy()
 
-    menuAccueil=True
-    menujouer=False
-    menuInscription=False
-    menuConnection=False
-    menuRejoindre=False
-    menuInviter=False
+#-------------------------------------------------- Partie Inscription ----------------------------------------------------#
 
+def SignUp():
+    global entry_pseudo, entry_mdp, entry_Vérif, Submit, Pseudo, mdp, Vérif_mdp
+    dessin.itemconfigure( titre1 , text="Inscription")
+    Pseudo = Label(fenetre, text="Pseudo",width=20,font=("bold", 10))
+    Pseudo.place(x=500,y=300)
+    entry_pseudo = Entry(fenetre)
+    entry_pseudo.place(x=700,y=300)
+    mdp = Label(fenetre, text="Mot de Passe",width=20,font=("bold", 10))
+    mdp.place(x=500,y=350)
+    entry_mdp = Entry(fenetre)
+    entry_mdp.place(x=700,y=350)
+    Vérif_mdp = Label(fenetre, text="Vérification Mot de Passe",width=20,font=("bold", 10))
+    Vérif_mdp.place(x=500,y=400)
+    entry_Vérif = Entry(fenetre)
+    entry_Vérif.place(x=700,y=400)
+    Submit = Button(fenetre, text='Submit',width=20,bg='red',fg='white', command=executSignUp).place(x=600,y=450)
 
-def inscription():
-    signup
+    boutJouer.destroy()
+    boutInscription.destroy()
+    boutConnection.destroy()
+    boutquitter.destroy()
+
+def executSignUp():
+    global entry_pseudo, entry_mdp, entry_Vérif, Submit
+    if entry_pseudo.get()=="" or entry_mdp.get()=="" or entry_Vérif.get()=="":
+        messagebox.showerror("Error" , "All Fields Are Required" , parent = fenetre)
+    if entry_mdp.get() != entry_Vérif.get():
+        messagebox.showerror("Error" , "Password & Confirm Password Should Be Same" , parent = fenetre)
+    else:
+        try:
+            print("Je suis dans la fonction")
+            con = sqlite3.connect('BDD.db')
+            print("Connection réussie")
+            cur = con.cursor()
+            # cur.execute("select * from user where Pseudo=?",entry_pseudo.get())
+            print("Select OK")
+            row = cur.fetchone()
+            print("Test 2")
+            if row!=None:
+                messagebox.showerror("Error" , "User Name Already Exits", parent = fenetre)
+            else:
+                print("test avant insert")
+                cur.execute("insert into user(Pseudo,HashPassword) values(?,?)",(entry_pseudo.get(), entry_mdp.get()))
+                print("test après insert")
+                con.commit()
+                con.close()
+                messagebox.shofenetrefo("Success" , "Registration Successfull" , parent = fenetre)
+            
+        except Exception as es:
+            print("Erreur Exception")
+            messagebox.showerror("Error" , f"Error : {str(es)}", parent = fenetre)
+
+    accueil()
+
+#-------------------------------------------------- Partie Connection ----------------------------------------------------#
 
 def connection():
-    login
+    global username, password, passentry, btn_login
+    username = Label(fenetre, text= "User Name :" , font='Verdana 10 bold')
+    username.place(x=60,y=220)
+    password = Label(fenetre, text= "Password :" , font='Verdana 10 bold')
+    password.place(x=60,y=260)
+    userentry = Entry(fenetre, width=40 , textvariable = username)
+    userentry.focus()
+    userentry.place(x=200 , y=223)
+    passentry = Entry(fenetre, width=40, show="*" ,textvariable = password)
+    passentry.place(x=200 , y=260)
+
+    # button login and clear
+    btn_login = Button(fenetre, text = "Login" ,font='Verdana 10 bold',command = executLogin)
+    btn_login.place(x=200, y=293)
+
+    boutJouer.destroy()
+    boutInscription.destroy()
+    boutConnection.destroy()
+    boutquitter.destroy()
+
+def executLogin():
+    global username, password, passentry, btn_login
+    if username.get()=="" or password.get()=="":
+        messagebox.showerror("Error","Enter User Name And Password",parent=fenetre)	
+    else:
+        try:
+            con = sqlite3.connect('BDD.db')
+            cur = con.cursor()
+            
+            cur.execute("SELECT * FROM user WHERE Pseudo=? and HashPassword =?",(username.get(),password.get()))
+            row = cur.fetchone()
+
+            if row==None:
+                messagebox.showerror("Error" , "Invalid User Name And Password", parent = fenetre)
+
+            else:
+                messagebox.shofenetrefo("Success" , "Successfully Login" , parent = fenetre)
+            con.close()
+        except Exception as es:
+            messagebox.showerror("Error" , f"Error : {str(es)}", parent = fenetre)
+    accueil()
 
 def jouer():
-    global boutRejoindre, boutInvite, boutRetour, menujouer, menuInscription, menuAccueil, menuConnection, menuRejoindre, menuInviter
+    global boutRejoindre, boutInvite, boutRetour
     dessin.itemconfigure( titre1 , text="Jouer")
 
     boutRejoindre = Button(fenetre,text='Rejoindre', width =11, command=rejoindre, font=ftComic,bg="#D1D1D1",fg="#FF9E3D")
@@ -49,16 +143,21 @@ def jouer():
     boutInscription.destroy()
     boutConnection.destroy()
     boutquitter.destroy()
-
-    menuAccueil=False
-    menujouer=True
-    menuInscription=False
-    menuConnection=False
-    menuRejoindre=False
-    menuInviter=False
+    boutOK.destroy()
+    boutRetour2.destroy()
 
 def rejoindre():
-    pass
+    global boutRejoindre, boutInvite, boutOK, boutRetour, boutRetour2
+    dessin.itemconfigure( titre1 , text="Rejoindre")
+
+    boutOK = Button(fenetre,text='Valider', width =11, command=jouer, font=ftComic,bg="#D1D1D1",fg="#FF9E3D")
+    boutOK.grid (row =7,column =10, padx = 10,pady = 10, columnspan =3)
+    boutRetour2 = Button(fenetre,text='Retour', width =11, command=jouer, font=ftComic,bg="#D1D1D1",fg="#FF9E3D")
+    boutRetour2.grid (row =7,column =2, padx = 10,pady = 10, columnspan =3)
+
+    boutRejoindre.destroy()
+    boutInvite.destroy()
+    boutRetour.destroy()
 
 def invite():
     pass
@@ -82,7 +181,7 @@ ftComic3 = Font (family = 'Comic Sans MS', size = -20, underline = False, weight
 selfX1,selfY1=Xmax/2,40;100
 titre1= dessin.create_text(selfX1,selfY1,text='Projet Matchmaking',fill='Red', font= ftComic2)
 
-boutInscription = Button(fenetre,text='Inscription', width =11, command=inscription, font=ftComic,bg="#D1D1D1",fg="#FF9E3D")
+boutInscription = Button(fenetre,text='Inscription', width =11, command=SignUp, font=ftComic,bg="#D1D1D1",fg="#FF9E3D")
 boutInscription.grid (row =12,column =6, padx = 10,pady =5, columnspan =3)
 boutConnection = Button(fenetre,text='Connection', width =11, command=connection, font=ftComic,bg="#D1D1D1",fg="#FF9E3D")
 boutConnection.grid (row = 13,column = 6, padx = 10,pady = 5, columnspan =3)
